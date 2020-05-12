21e9f366ef6efd54fb03ddafd91ebd75c6fb4c9d
Patryk Rygiel <patrygiel99@gmail.com>
2020-05-12 16:38:23 +0000

    Added: A pretty long list here: * Divided client hierarchy by adding few more directories: object (for ObjectManager and all objects stuff), resources (here we have ResourcesManager class and textures folder inside) and collision (almost empty now, content to be added) * Created a resource management system (see ResourcesManager.py), which loads all resources from disk and makes them able to be used in the future * Added Object class, which should be the base class for all objects on the scene. Also added basic objects types: ObjectSurface to represent a grass pane and an ObjectWall for a simple wall * Each object is registered by the ObjectManager while being instantiated. It takes care of updating and rendering all of registered objects - so, in the future add all of the behaviour into the update method * Created a simple map, but just for test purposes

--- client/scenes/game_scene.py
+++ client/scenes/game_scene.py
@@ -6,6 +6,7 @@ from client.utils import flipy
 from client.maps.map import Map
 from client.objects.ball import Ball
 from client.scenes.scene import Scene
+from client.enums.ball_state_enum import BallState
 
 
 class GameScene(Scene):
@@ -19,14 +20,34 @@ class GameScene(Scene):
             for i, color in enumerate(player)
         ]
 
+        self.trajectory = None
+        self.next_turn = False
+
         self.balls[0].turn = True
         print("Player 0 to move")
 
     def draw(self, screen):
+        """Draw scene and change ball if required"""
+
+        # Draw map
         self.map.draw(screen)
 
+        # Draw balls and check if in a cup
         for ball in self.balls:
+            if self.map.cup.ball_in(ball.body.position) and ball.state is not BallState.IN_CUP:
+                print("Ball {} in a cup".format(ball.index))
+                self.remove(ball)
+
             ball.draw(screen)
+
+        # Draw trajectory when aiming
+        if self.trajectory:
+            pygame.draw.lines(screen, (255, 0, 230), False, self.trajectory, 2)
+
+        # Switch turn
+        if self.balls_not_moving():
+            if self.next_turn or self.current_ball().state is BallState.IN_CUP:
+                self.next_ball()
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -35,30 +56,97 @@ class GameScene(Scene):
                 ball = self.clicked_ball(Vec2d(event.pos[0], flipy(event.pos[1])))
 
                 if ball:
-                    if ball.turn and not ball.stroke and not ball.moving:
-                        ball.stroke = True
+                    # If its ball's turn and all balls are still, let the ball be clicked to hit
+                    if ball.turn and self.balls_not_moving():
+                        pos = ball.body.position
+                        self.trajectory = [(pos.x, flipy(pos.y)), (pos.x, flipy(pos.y))]
+                        ball.state = BallState.CLICKED
 
         elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == pygame.BUTTON_LEFT:
-                # Get current ball if none than omit
-                ball = self.current_ball()
+            # Get current ball
+            ball = self.current_ball()
 
-                if ball:
-                    ball.stroke = False
-                    ball.moving = True
+            if ball.state is BallState.CLICKED:
+                self.trajectory = None
+
+                if event.button == pygame.BUTTON_LEFT:
                     direction = 5*Vec2d(
                         ball.body.position.x - event.pos[0],
                         ball.body.position.y - flipy(event.pos[1])
                     )
                     ball.body.apply_impulse_at_local_point(direction)
+                    self.next_turn = True
+                else:
+                    ball.state = BallState.NOT_MOVING
+
+        elif event.type == pygame.MOUSEMOTION:
+            if self.trajectory:
+                self.trajectory[1] = pygame.mouse.get_pos()
+
+    def remove(self, ball):
+        """Removing ball from space"""
+        ball.state = BallState.IN_CUP
+        self.space.remove(ball.shape, ball.body)
 
     def clicked_ball(self, pos):
+        """Get clicked ball, only if its turn"""
         for ball in self.balls:
-            if ball.is_clicked(pos):
+            if ball.is_clicked(pos) and ball.turn:
                 return ball
 
         return None
 
     def current_ball(self):
-        ball = [ball for ball in self.balls if ball.turn is True and ball.stroke is True]
-        return None if len(ball) <= 0 else ball[0]
+        """Get current ball to strike"""
+        ball = [ball for ball in self.balls if ball.turn]
+        return ball[0]
+
+    def next_ball(self):
+        """Change turn for next ball"""
+        current_ball = self.current_ball()
+        current_ball.turn = False
+
+        # Choose next player
+        tries = len(self.balls)
+        next_id = (self.balls.index(current_ball) + 1) % len(self.balls)
+
+        while self.balls[next_id].state is BallState.IN_CUP and tries > 0:
+            next_id = (next_id + 1) % len(self.balls)
+            tries -= 1
+
+        if tries > 0:
+            self.balls[next_id].turn = True
+            self.next_turn = False
+            print("Player {} to move".format(next_id))
+
+        # If all players ended, load next map
+        else:
+            print("Next map")
+            self.next_map()
+
+    def next_map(self):
+        """Switch to next map"""
+
+        # Currently showing the same map again
+        self.map = Map(1, self.space)
+
+        # Resetting balls and adding them back to simulation space
+        for i, ball in enumerate(self.balls):
+            ball.state = BallState.NOT_MOVING
+            ball.body.position = Vec2d(300, 540 + 10 * i)
+            ball.body.velocity = Vec2d(0.0, 0.0)
+            self.space.add(ball.shape, ball.body)
+
+        self.next_turn = False
+
+        # Setting up first player
+        self.balls[0].turn = True
+        print("Player 0 to move")
+
+    def balls_not_moving(self):
+        """Check if all balls are still"""
+        for ball in self.balls:
+            if ball.state is BallState.MOVING or ball.state is BallState.CLICKED:
+                return False
+
+        return True
--- client/utils.py
+++ client/utils.py
@@ -1,3 +1,8 @@
 def flipy(y):
     """Converting pymunk coordinates to pygame"""
     return -y + 900
+
+
+def sign(x):
+    """Getting plus or minus sign of given value"""
+    return 1 if x >= 0 else -1
